530d30c4023d00fcf979990f12a05acbf35e58c8
Kaira <livingbetweenrainbows@gmail.com>
2017-02-19 00:43:54 +0800

    Edited stuff
    Hello life, I edited stuff

--- app/assets/javascripts/angular/controllers/dashboard_controller.js
+++ app/assets/javascripts/angular/controllers/dashboard_controller.js
@@ -1,11 +1,15 @@
-app.controller('DashboardController', ['$scope', 'DemoService',
-  function($scope, DemoService) {
+app.controller('DashboardController', ['$scope', 'DemoService', '$state',
+  function($scope, DemoService, $state) {
     this.showNav = false;
 
     $scope.toggle = function() {
       this.showNav= !this.showNav;
     }
 
+
+    $scope.apply = function() {
+      $state.go('.sidebar.subnavbar.apply');
+    }
     $scope.scholarships = DemoService.scholarships();
   }
 
