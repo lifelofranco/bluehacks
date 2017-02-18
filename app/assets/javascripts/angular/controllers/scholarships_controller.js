c2118b091bdcbf59404d1efe05dc1567c472d369
Life Christian <lifelofranco@gmail.com>
2017-02-19 02:17:10 +0800

    added search

--- app/assets/javascripts/angular/controllers/scholarships_controller.js
+++ app/assets/javascripts/angular/controllers/scholarships_controller.js
@@ -2,11 +2,26 @@ app.controller('ScholarshipsController', ['$scope',
   function($scope) {
     this.showNav = false;
     $scope.$parent.title = 'Search Scholarships';
-
+    $scope.showTopics = function() {
+      this.topics = !this.topics;
+    };
 
     $scope.toggle = function() {
       this.showNav= !this.showNav;
     }
+
+      $scope.topic_filters = [
+      {"label": "All Locations",
+       "value": "all"},
+      {"label": "Manila",
+       "value": "manila"},
+      {"label": "Cebu",
+       "value": "cebu"},
+      {"label": "Davao",
+      "value": "marketing"},
+      {"label": "Zamboanga",
+       "value": "zamboanga"}
+    ]
   }
 
 
